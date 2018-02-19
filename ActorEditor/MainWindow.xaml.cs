fa22ff5ec32c76260c543f7c0816358f4dd603b2
Stanislas Dolcini <stanislas.dolcini@devinci.fr>
2018-02-19 15:12:45 +0100

    Remove useless usings

--- ActorEditor/MainWindow.xaml.cs
+++ ActorEditor/MainWindow.xaml.cs
@@ -1,25 +1,14 @@
-﻿using ActorEditor.Model;
-using ActorEditor.Model.Entities;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ActorEditor
+﻿namespace ActorEditor
 {
+    using ActorEditor.Model;
+    using ActorEditor.Model.Entities;
+    using Microsoft.Win32;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
