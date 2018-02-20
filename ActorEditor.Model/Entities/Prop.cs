0c56c009d507b96ad8992a830ed5f1e9974f9202
Stanislas Dolcini <stanislas.dolcini@devinci.fr>
2018-02-20 12:05:39 +0100

    Add prop support

--- ActorEditor.Model/Entities/Prop.cs
+++ ActorEditor.Model/Entities/Prop.cs
@@ -12,26 +12,46 @@ namespace ActorEditor.Model
         private string _relativePath;
         private decimal _maxHeight;
         private decimal _minHeight;
-        private bool IsSelectable;
+        private bool _IsSelectable;
+
+        public Prop()
+        {
+
+        }
 
         public Prop(XElement prop)
         {
             this._relativePath = prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "actor")?.Value;
             this.AttachPoint = prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "attachpoint")?.Value;
             decimal.TryParse(prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "minheight")?.Value, out this._minHeight);
-            decimal.TryParse(prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "minheight")?.Value, out this._maxHeight);
-            bool.TryParse(prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "selectable")?.Value, out this.IsSelectable);
+            decimal.TryParse(prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "maxheight")?.Value, out this._maxHeight);
+            bool.TryParse(prop.Attributes().FirstOrDefault(a => a.Name.LocalName == "selectable")?.Value, out this._IsSelectable);
         }
 
-        public bool IsSelectable1 { get => IsSelectable; set => IsSelectable = value; }
+        internal XElement SerializeElements()
+        {
+            var Xprop = new XElement("prop");
+            if (!string.IsNullOrEmpty(this.attachPoint))
+                Xprop.Add(new XAttribute("attachpoint", this.attachPoint));
+            if (!string.IsNullOrEmpty(this._relativePath))
+                Xprop.Add(new XAttribute("actor", this._relativePath));
+            if (_IsSelectable)
+                Xprop.Add(new XAttribute("selectable", 1));
+            if (_minHeight != 0)
+                Xprop.Add(new XAttribute("minheight", _minHeight));
+            if (_maxHeight != 0)
+                Xprop.Add(new XAttribute("maxheight", _maxHeight));
+            return Xprop;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsChecked { get; set; }
+        public bool IsSelectable { get => _IsSelectable; set => _IsSelectable = value; }
         public decimal MinHeight { get => _minHeight; set => _minHeight = value; }
         public string AttachPoint { get => attachPoint; set => attachPoint = value; }
-
-        public string GetRelativePath()
-        { return this._relativePath; }
-
-        public void SetRelativePath(string value)
-        { this._relativePath = value; }
-
+        public string RelativePath { get => _relativePath; set => _relativePath = value; }
+        public decimal MaxHeight { get => _maxHeight; set => _maxHeight = value; }
     }
 }
--- ActorEditor.Model/Entities/Props.cs
+++ ActorEditor.Model/Entities/Props.cs
@@ -18,25 +18,14 @@ namespace ActorEditor.Model.Entities
                     this.Add(new Prop(prop));
         }
 
-        public override string ToString()
-        {
-            string serializedString = string.Empty;
-            foreach (Prop prop in this)
-            {
-                serializedString += "(" + prop.GetRelativePath() + ", " + prop.AttachPoint + ")";
-            }
-            return !string.IsNullOrEmpty(serializedString) ? (serializedString.Substring(0, 10 > serializedString.Length ? 10 : serializedString.Length)) : string.Empty;
-        }
+        public override string ToString() => (this.Count > 0 ? this.Count.ToString() + " prop(s)" : "No prop");
 
         public XElement SerializeElements()
         {
             var props = new XElement("props");
             foreach (var prop in this)
             {
-                var Xprop = new XElement("prop");
-                Xprop.Add(new XAttribute("attachpoint", prop.AttachPoint));
-                Xprop.Add(new XAttribute("actor", prop.GetRelativePath()));
-                props.Add(Xprop);
+                props.Add(prop.SerializeElements());
             }
             return props;
         }
