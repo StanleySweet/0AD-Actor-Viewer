e6eeb25939523ca551bf95d77990e17a1484e100
Stanislas Dolcini <stanislas.dolcini@devinci.fr>
2018-02-21 16:12:34 +0100

    Add a converter.

---
+++ ActorEditor/Converters/TextureTypeToVisibility.cs
@@ -0,0 +1,23 @@
+﻿using ActorEditor.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ActorEditor.Converters
+{
+    class TextureTypeToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,  CultureInfo culture)
+        {
+            return ((TextureType)value);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
